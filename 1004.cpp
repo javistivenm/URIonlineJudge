44957b864d2d92f4ca04e6962056f2f63908c044
Javier Stiven Murillo <javistivenmurillo@gmail.com>
2020-03-14 21:47:10 -0500

    Add files via upload

---
+++ 1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int valor1, valor2, PROD;
+    cin >> valor1 >> valor2;
+    PROD = valor1 * valor2;
+    cout << "PROD = "<< PROD;
+    cout <<"\n";
+
+    return 0;
+}
