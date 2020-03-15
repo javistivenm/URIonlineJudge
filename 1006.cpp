9dc08a21344d56c956f2308f34776a6f21373fe0
Javier Stiven Murillo <javistivenmurillo@gmail.com>
2020-03-14 21:47:53 -0500

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    double A,B, C, MEDIA;
+    cin >> A >> B >> C;
+    MEDIA = (A*2 + B*3 + C*5) / 10;
+    cout << "MEDIA = " << fixed << setprecision(1) << MEDIA;
+    cout << "\n";
+    return 0;
+}
